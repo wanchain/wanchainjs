7233ce530b19802b8820d95e3806112fb76492b3
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-07-27 19:05:12 +0800

    update docker script, restart container

--- dev_setup.sh
+++ dev_setup.sh
@@ -3,7 +3,7 @@
 
 docker inspect wanchainjsdev > /dev/null 2>&1
 if [ $? -eq 1 ]; then
-		docker run -itd --network=host --name wanchainjsdev registry.cn-hangzhou.aliyuncs.com/wanglutech/wanchainjs /bin/sh
+		docker run -itd --restart always --network=host --name wanchainjsdev registry.cn-hangzhou.aliyuncs.com/wanglutech/wanchainjs /bin/sh
 	fi
 docker exec -it wanchainjsdev /bin/sh
 
