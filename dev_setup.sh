19172043bd4becb9841f5cb5300e8ff1e261b7ac
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-07-27 11:16:36 +0800

    update docker script

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-docker run -it --network=host --rm registry.cn-hangzhou.aliyuncs.com/wanglutech/wanchainjs /bin/sh
+
+docker inspect wanchainjsdev > /dev/null 2>&1
+if [ $? -eq 1 ]; then
+		docker run -itd --network=host --name wanchainjsdev registry.cn-hangzhou.aliyuncs.com/wanglutech/wanchainjs /bin/sh
+	fi
+docker exec -it wanchainjsdev /bin/sh
+
+
