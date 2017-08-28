fc4478c658954c54f8920882f0ef8b7cf7816716
Zhang Lihua <zhang.lihua@wanglutech.com>
2017-08-28 10:13:29 +0800

    update dev_setup

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 
+curdir=`pwd`
 docker inspect wanchainjsdev > /dev/null 2>&1
 if [ $? -eq 1 ]; then
-		docker run -itd --restart always --network=host --name wanchainjsdev registry.cn-hangzhou.aliyuncs.com/wanglutech/wanchainjs /bin/sh
+		docker run -itd --restart always --network=host --name wanchainjsdev -v "${curdir}/jsutility:/wanchainjs" registry.cn-hangzhou.aliyuncs.com/wanglutech/wanchainjs /bin/sh
 	fi
 docker exec -it wanchainjsdev /bin/sh
 
